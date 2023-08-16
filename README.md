6638119c502193d0b1dedd82b34acfec46cd4bf5
Kordrad <kord.stp@gmail.com>
2023-08-16 12:12:32 +0200

    feat(cli): create simple CLI to install packages
    includes:
    - cli "startly" command
    - build
    - project configuration

---
+++ README.md
@@ -0,0 +1,37 @@
+# Startly
+
+A streamlined tool designed to accelerate frontend project setup
+
+## Usage/Examples
+
+```bash
+$ startly
+```
+
+## Run Locally
+
+#### Global installation
+
+```bash
+npm run build
+npm i -g .
+```
+
+#### Local run
+
+```bash
+npm run startly
+```
+
+#### run in another project
+
+```bash
+cd path/to/your/project
+startly
+```
+
+## Requirements
+
+System: Windows
+
+Package menager: NPM
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,41 @@
-//
-console.info('initial');
+import * as cp from 'node:child_process';
+
+async function loadMyModule() {
+  const { default: inquirer } = await import('inquirer');
+
+  inquirer
+    .prompt([
+      {
+        type: 'checkbox',
+        message: 'packages to install:',
+        name: 'packages',
+        choices: [
+          new inquirer.Separator('Commitlint'),
+          { name: '@commitlint/cli' },
+          { name: '@commitlint/config-conventional' },
+          new inquirer.Separator('Other'),
+          { name: 'husky' },
+          { name: 'lint-staged' },
+        ],
+      },
+    ])
+    .then(answers => {
+      cp.exec(
+        `npm i -D ${answers.packages.join(' ')}`,
+        (error: cp.ExecException | null, stdout: string, stderr: string) => {
+          if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+          }
+          if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+          }
+          console.log(stdout);
+        }
+      );
+    });
+}
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+loadMyModule().then();
