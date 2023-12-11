cbbc8827b4f68b8fddd327285f330f412895b3e1
Kordrad <kord.stp@gmail.com>
2023-12-11 15:30:54 +0100

    feat(cli/enquirer): categorize prompts and improve choices (#46)
    - remove disabled property in choices for installed packages
    - separate choices for categories:
      - code style
      - automations
      - linters

---
+++ packages/cli/src/components/prompts/multiselect.prompt.ts
@@ -0,0 +1,24 @@
+import enquirer from 'enquirer';
+
+import type { Choice } from '../../type/interfaces/choice.interface.js';
+import type { Prompt } from '../../type/interfaces/prompt.interface.js';
+
+function normalizeChoices(normalizeArray: Choice[]): Choice[] {
+  // eslint-disable-next-line unicorn/no-array-reduce
+  return normalizeArray.reduce((previous: Choice[], currentValue: Choice) => {
+    const { choices = [], ...rest } = currentValue;
+    const nextValue = [
+      rest,
+      ...(choices?.length > 0 ? normalizeChoices(choices) : []),
+    ];
+    return [...previous, ...nextValue];
+  }, []);
+}
+
+export const MultiSelect = async <T>(options: Omit<Prompt, 'type'>) =>
+  await enquirer.prompt<T>({
+    ...options,
+    ...(typeof options.result === 'function' && { result: options.result }),
+    type: 'multiselect',
+    choices: normalizeChoices(options.choices),
+  });
