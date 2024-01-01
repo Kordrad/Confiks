79219887f463c43b7bfe02c674c544dbc5f7ae3e
Kordrad <kord.stp@gmail.com>
2024-01-01 12:40:22 +0100

    refactor(package-json.utils): improve logic (#60)

--- packages/cli/src/utils/package-json.utils.ts
+++ packages/cli/src/utils/package-json.utils.ts
@@ -1,5 +1,13 @@
 import { fileSystem } from '../services/node/file-system.service.js';
 
 export function packageIsInstalled(packageName: string): boolean {
-  return fileSystem.readFileSync('./package.json').includes(packageName);
+  const packageJson = JSON.parse(
+    fileSystem.readFileSync('./package.json') ?? '{}'
+  );
+  const packages = Object.keys({
+    ...packageJson?.dependencies,
+    ...packageJson?.devDependencies,
+    ...packageJson?.peerDependencies,
+  });
+  return packages.includes(packageName);
 }
