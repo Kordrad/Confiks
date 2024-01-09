70c3f5a7430952d34cda55b8ac9e3254f4814cc3
Kordrad <kord.stp@gmail.com>
2024-01-09 21:23:34 +0100

    feat(yarn): add support for package manager

---
+++ packages/cli/src/type/enums/installation-type.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * @see https://pnpm.io/cli/add
+ * @see https://classic.yarnpkg.com/en/docs/cli/add#toc-commands
+ * */
+export enum InstallationType {
+  dependency = '',
+  developmentDependency = '-D',
+  global = '--global',
+}
