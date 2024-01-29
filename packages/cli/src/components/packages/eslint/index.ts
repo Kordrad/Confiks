4b87323f903ed66ce71e3f27ce1dd42a609b9a50
Kordrad <kord.stp@gmail.com>
2024-01-29 20:05:53 +0100

    ci(update-check): automatically check packages to update (#82)

---
+++ packages/cli/src/components/packages/eslint/index.ts
@@ -0,0 +1,5 @@
+export * from './eslint.package.js';
+export * from './eslint-plugin-prettier/eslint-plugin-prettier.package.js';
+export * from './eslint-plugin-simple-import-sort/eslint-plugin-simple-import-sort.package.js';
+export * from './eslint-plugin-unicorn/eslint-plugin-unicorn.package.js';
+export * from './eslint-plugin-unused-imports/eslint-plugin-unused-imports.package.js';
---
+++ packages/cli/src/components/packages/prettier/index.ts
@@ -0,0 +1,2 @@
+export * from './prettier.package.js';
+export * from './prettier-plugin-organize-attributes/prettier-plugin-organize-attributes.package.js';
