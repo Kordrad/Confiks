384df026c5f3a3ef21f8a32c3e49514c4c9acdc5
Kordrad <kord.stp@gmail.com>
2024-04-23 12:55:07 +0200

    style(package-scripts): remove 'all' word in scripts

--- packages/cli/src/constants/package-scripts-cli.constant.ts
+++ packages/cli/src/constants/package-scripts-cli.constant.ts
@@ -1,14 +1,14 @@
 export const ESLINT_SCRIPTS = {
-  'eslint:all': 'eslint **/*',
-  'eslint:all::fix': 'eslint **/* --fix',
+  eslint: 'eslint **/*',
+  'eslint:fix': 'eslint **/* --fix',
 };
 
 export const PRETTIER_SCRIPTS = {
-  'prettier:all': 'prettier --ignore-unknown **/*',
-  'prettier:all::write': 'prettier --write --ignore-unknown **/*',
+  prettier: 'prettier --ignore-unknown **/*',
+  'prettier:write': 'prettier --write --ignore-unknown **/*',
 };
 
 export const STYLELINT_SCRIPTS = {
-  'stylelint:all': 'stylelint **/*.{css,scss}',
-  'stylelint:all::fix': 'stylelint **/*.{css,scss} --fix',
+  stylelint: 'stylelint **/*.{css,scss}',
+  'stylelint:fix': 'stylelint **/*.{css,scss} --fix',
 };
