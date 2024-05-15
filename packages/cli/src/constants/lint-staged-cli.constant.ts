9fa9b1b418b63fb4d0b98df347608497e1426409
Kordrad <kord.stp@gmail.com>
2024-05-15 08:15:12 +0200

    refactor(cli/eslint): remove --quiet

--- packages/cli/src/constants/lint-staged-cli.constant.ts
+++ packages/cli/src/constants/lint-staged-cli.constant.ts
@@ -1,3 +1,3 @@
 export const PRETTIER_FIX = 'prettier --write --ignore-unknown';
-export const ESLINT_FIX = 'eslint --quiet --fix';
+export const ESLINT_FIX = 'eslint --fix';
 export const STYLELINT_FIX = 'stylelint --fix';
